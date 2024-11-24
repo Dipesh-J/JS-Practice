4629469336f0a66c89c32608738109ee6c0ce0ff
Dipesh-J <dipeshjoshi015@gmail.com>
2024-11-24 20:31:36 +0530

    two pointers

---
+++ DSA/Strings_Arrays/algorithms/twoPointer.js
@@ -0,0 +1,26 @@
+/*
+* Two Pointers
+
+* In this method we use two pointers to traverse the data structure in a synchronized manner. Generally in 3 ways:
+
+* 1. Opposite Ends
+* 2. Same Direction
+* 3. Fixed and moving
+
+? When do we use two pointers?
+* 1. Problems involving sorted array or strings.
+* 2. Finding pairs or triplets that satisfy some condition.
+* 3. Solving problems with subarray sums or subsequences.
+* 4. Searching for elements in a specific intervals.
+
+TODO: On Leetcode
+* 1. Two Sum
+* 2. Remove Duplicates from a sorted array
+* 3. Checking for Palindrome
+* 4. Longest Substring Without Repeating Characters
+*/
+
+const nums = [2, 7, 11, 15],
+  target = 9;
+
+function twoSum(nums, target) {}
